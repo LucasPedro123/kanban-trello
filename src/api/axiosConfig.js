16849bbf5cfe9cf8e47b9df60d05b4cfd9390374
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-04-29 04:07:43 -0300

    fix: Changing the server address

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Configuração básica do axios
 const api = axios.create({
-  baseURL: 'http://localhost:5000', // URL do backend
+  baseURL: 'https://system-trello-register.onrender.com', // URL do backend
   headers: {
     'Content-Type': 'application/json',
   },
